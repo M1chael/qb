5b6d8c765cecfb05a8ea032f4c70d9493ce102d8
M1chael <asoi@mail.ru>
2018-01-20 11:23:03 +0500

    refactoring

--- lib/bot.rb
+++ lib/bot.rb
@@ -5,17 +5,19 @@ class Bot
     options.each{ |key, value| instance_variable_set("@#{key}", value) }
 	end
 
-  def markup(thx)
-    kb = [[Telegram::Bot::Types::InlineKeyboardButton.new(text: thx.to_s, callback_data: 'rating'), 
-      Telegram::Bot::Types::InlineKeyboardButton.new(text: @vote, callback_data: 'thx')]]
-    Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)    
-  end
-
   def post(quote)
     Telegram::Bot::Client.run(@token) do |telegram| 
       @response = telegram.api.send_message(chat_id: @chat_id, text: quote.text, reply_markup: markup(quote.score))
     end
     DB[:quotes].where(id: quote.id).update(post_count: quote.post_count + 1, post_date: Time.now.to_i)
     DB[:messages].insert(mid: @response['result']['message_id'], qid: quote.id)
+  end
+
+  private
+
+  def markup(thx)
+    kb = [[Telegram::Bot::Types::InlineKeyboardButton.new(text: thx.to_s, callback_data: 'rating'), 
+      Telegram::Bot::Types::InlineKeyboardButton.new(text: @vote, callback_data: 'thx')]]
+    Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)    
   end
 end
