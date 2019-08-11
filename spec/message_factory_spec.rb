1824b22de72210f7a3c1f63e42b7f965cea979e4
M1chael <asoi@mail.ru>
2019-08-11 15:30:03 +0500

    tests isolated from classes Quote and Post

--- spec/message_factory_spec.rb
+++ spec/message_factory_spec.rb
@@ -5,22 +5,25 @@ describe Message_factory do
   let(:factory) { Message_factory.new }
 
   describe '#get_message' do
+    before(:example) do
+      @post = instance_double(Post)
+      @quote = instance_double(Quote)
+      allow(Post).to receive(:new).and_return(@post)
+      allow(Quote).to receive(:new).and_return(@quote)
+    end
+
     it 'returns post' do
-      expect(factory.get_message(type: :post)).to be_a(Post)
+      expect(factory.get_message(type: :post)).to eq(@post)
     end
 
     it 'returns quote' do
-      expect(factory.get_message(type: :quote)).to be_a(Quote)
+      expect(factory.get_message(type: :quote)).to eq(@quote)
     end
 
     context 'when mid is in place instead of type' do
       before(:example) do
         @post_mid = 1
         @quote_mid = 2
-        @post = instance_double(Post)
-        @quote = instance_double(Quote)
-        allow(Post).to receive(:new).with(@post_mid).and_return(@post)
-        allow(Quote).to receive(:new).with(@quote_mid).and_return(@quote)
         DB[:messages].delete
         DB[:messages].insert(mid: @post_mid, eid: 2, type: 'post')
         DB[:messages].insert(mid: @quote_mid, eid: 3, type: 'quote')
